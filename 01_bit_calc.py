e47045a5f54477247dafa48ccb4fc44904b7e98e
normans1011 <101604933+normans1011@users.noreply.github.com>
2022-05-04 10:58:52 +1200

    Add files via upload
    pasted the integer checker in
    pasted the input checker into bit calc
    labled

--- 01_bit_calc.py
+++ 01_bit_calc.py
@@ -1,7 +1,22 @@
-#checks user choice is 'integer', 'text', or 'image'
-from re import M
 
+# Functions go here
 
+# Puts a series of symbols at the start and end of the text
+def statement_generator(text, decoration):
+
+    # make string with five characters
+    ends = decoration * 5
+
+    # add decoration to start and end of statement
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+# Checks if user choice is 'integer', 'text', or 'image'
 def user_choice():
 
     #list of valid responses
@@ -38,10 +53,32 @@ def user_choice():
             print("Please choose a valid file type!")
             print()
 
-#main routine goes here
-data_type = user_choice()
-print ("You chose", data_type)
-print()
+# Main routine goes here
+statement_generator("hello world", "-")
+
+# Heading
+
+
+# Display instructions if user has not used the program before
+
+# Loop to allow multiple calculations per session
+keep_going = ""
+while keep_going=="":
+
+    # Ask the user for the file type
+    data_type = user_choice()
+    print ("You chose", data_type)
+
+    # For integers, ask for integer
+    # (must be an integer more than / equal to 0)
+
+
+    # For images ask width and height
+
+
+
+
+
 
 
 
